a6b8866b91ea795479f4c61ee0abeb96fe7d4ec8
ijbohr <bohreli@gmail.com>
2016-06-18 08:50:10 +0300

    Скрипт разнесен

---
+++ js/dictionary.js
@@ -0,0 +1,186 @@
+﻿function getDictionary() {
+return {
+                "verbs": [{
+                        "id": 0,
+                        "name": "bereit sein ...",
+                        "desc": "быть готовым к ...",
+                        "prep_id": 0,
+                        "example": "Ich habe das Buch gelernt und jetzt bin ich #zur Prüfung# bereit."
+                    }, {
+                        "id": 1,
+                        "name": "sich beschäftigen ...",
+                        "desc": "заниматься с ...",
+                        "prep_id": 1,
+                        "example": "Er beschäftigt sich #mit den Kindern#."
+                    }, {
+                        "id": 2,
+                        "name": "sich beziehen ...",
+                        "desc": "ссылаться на ...",
+                        "prep_id": 2,
+                        "example": "Ich beziehe mich #auf Ihren Brief# vom 23. September."
+                    }, {
+                        "id": 3,
+                        "name": "bitten ...",
+                        "desc": "просить о ...",
+                        "prep_id": 3,
+                        "example": "Er hat mich #um einen Gefallen# gebeten."
+                    }, {
+                        "id": 4,
+                        "name": "danken ...",
+                        "desc": "благодарить за ...",
+                        "prep_id": 4,
+                        "example": "Ich danke Ihnen #für die Einladung#."
+                    }, {
+                        "id": 5,
+                        "name": "denken ...",
+                        "desc": "помнить о ...",
+                        "prep_id": 5,
+                        "example": "Denkst du #an deine Reise# nach Deutschland in diesem Jahr?"
+                    }, {
+                        "id": 6,
+                        "name": "einen Eindruck ... haben/bekommen",
+                        "desc": "иметь/получить впечатление от ...",
+                        "prep_id": 6,
+                        "example": "Bekommen Sie einen guten Eidruck #vom Theatherbesuch#?"
+                    }, {
+                        "id": 7,
+                        "name": "fahren ...",
+                        "desc": "ехать с .../на ...",
+                        "prep_id": 1,
+                        "example": "Wenn du nach Moskau #mit dem Auto# fahren möchtest, fahr bitte #mit mir#!"
+                    }, {
+                        "id": 8,
+                        "name": "fragen ...",
+                        "desc": "спрашивать о ...",
+                        "prep_id": 7,
+                        "example": "Ich finde mich in der Stadt schlecht zurecht. Fragen Sie bitte einen Passanten #nach dem Weg#!"
+                    }, {
+                        "id": 9,
+                        "name": "sich freuen ... (jetzt)",
+                        "desc": "радоваться ... (нынешнему)",
+                        "prep_id": 8,
+                        "example": "Heute ist mein Geburtstag und ich freue mich #über die Geschenke#."
+                    }, {
+                        "id": 10,
+                        "name": "sich freuen ... (in Zukunft)",
+                        "desc": "радоваться ... (будущему)",
+                        "prep_id": 2,
+                        "example": "-Du siehst sehr glücklich aus. -Ja, ich freuhe mich #auf den Urlaub#: in einem Monat fahre ich nach Österreich."
+                    }, {
+                        "id": 11,
+                        "name": "es handelt sich ...",
+                        "desc": "речь идет о ...",
+                        "prep_id": 3,
+                        "example": "In diesem Brief handelt es sich #um einen neuen Preis# fur die Ausrüstung, die wir lezte Woche bestellen hatten."
+                    }, {
+                        "id": 12,
+                        "name": "hoffen ...",
+                        "desc": "надеяться на ...",
+                        "prep_id": 2,
+                        "example": "Das ist ein großes Problem für unseren Betrieb, deswegen hoffe ich #auf Ihre baldige Antwort#."
+                    }, {
+                        "id": 13,
+                        "name": "sich interessieren ...",
+                        "desc": "интересоваться ...",
+                        "prep_id": 4,
+                        "example": "Ich interessiere mich #für diese Frau#. Wer ist sie?"
+                    }, {
+                        "id": 14,
+                        "name": "... interessiert sein",
+                        "desc": "в ... быть заинтересованным",
+                        "prep_id": 9,
+                        "example": "Wir haben viele Bestellungen, darum sind wir #an schneller Lieferung# der Ausrüstung interessiert."
+                    }, {
+                        "id": 15,
+                        "name": "... stolz sein",
+                        "desc": "... гордиться",
+                        "prep_id": 2,
+                        "example": "Wir haben die tausenden Werkzeugmaschine geliefert, deswegen sind wir #auf diesen Erfolg# stolz."
+                    }, {
+                        "id": 16,
+                        "name": "suchen ...",
+                        "desc": "искать ...",
+                        "prep_id": 7,
+                        "example": "Wenn du heute nichts findest, ich werde morgen #nach dem Buch# suchen."
+                    }, {
+                        "id": 17,
+                        "name": "teilnehmen ...",
+                        "desc": "принимать участие в ...",
+                        "prep_id": 9,
+                        "example": "Herr Orlow, wir hoffen, dass Sie Zeit haben, #an unserer Besprechung# teilzunehmen."
+                    }, {
+                        "id": 18,
+                        "name": "sich verabschieden ...",
+                        "desc": "прощаться с ...",
+                        "prep_id": 6,
+                        "example": "Haben Sie sich #von den Kollegen# verabschieden?"
+                    }, {
+                        "id": 19,
+                        "name": "warten ...",
+                        "desc": "ждать ...",
+                        "prep_id": 2,
+                        "example": "Ich warte hier #auf den Vertreter# der Firma \"Kulon\"."
+                    }, {
+                        "id": 20,
+                        "name": "sich wenden ...",
+                        "desc": "обращаться к ...",
+                        "prep_id": 5,
+                        "example": "Ich habe starken Kopfschmerzen. Ich werde mich #an einen Arzt# wenden."
+                    }, {
+                        "id": 21,
+                        "name": "zufrieden sein ...",
+                        "desc": "быть довольным ...",
+                        "prep_id": 1,
+                        "example": "Kommen Sie bitte rein! Ich bin #mit Ihrem Besuch# sehr zufrieden!"
+                    }, {
+                        "id": 22,
+                        "name": "zuständig sein ...",
+                        "desc": "быть ответственным за ...",
+                        "prep_id": 4,
+                        "example": "Ich kann nicht Ihnen helfen, weil ich #für diese Frage# nicht zuständig bin. Fragen Sie meinen Chef danach!"
+                    }
+                ],
+                "preps": [{
+                        "id": 0,
+                        "name": "zu",
+                        "case": "D"
+                    }, {
+                        "id": 1,
+                        "name": "mit",
+                        "case": "D"
+                    }, {
+                        "id": 2,
+                        "name": "auf",
+                        "case": "A"
+                    }, {
+                        "id": 3,
+                        "name": "um",
+                        "case": "A"
+                    }, {
+                        "id": 4,
+                        "name": "für",
+                        "case": "A"
+                    }, {
+                        "id": 5,
+                        "name": "an",
+                        "case": "A"
+                    }, {
+                        "id": 6,
+                        "name": "von",
+                        "case": "D"
+                    }, {
+                        "id": 7,
+                        "name": "nach",
+                        "case": "D"
+                    }, {
+                        "id": 8,
+                        "name": "über",
+                        "case": "A"
+                    }, {
+                        "id": 9,
+                        "name": "an",
+                        "case": "D"
+                    }
+                ]
+            }
+			}
